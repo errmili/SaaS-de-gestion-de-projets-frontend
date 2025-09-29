9fb6b540b6a9ace3d8746bae4c19903f3c843b31
errmili <96416928+errmili@users.noreply.github.com>
2025-09-29 14:35:47 +0100

    fix: use deployed auth service URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
-
 export const environment = {
   production: true,
-  authApiUrl: 'https://saas-de-gestion-de-projets-type-jira-asana--1.onrender.com',
-  projectApiUrl: 'https://saas-de-gestion-de-projets-type-jira-asana-.onrender.com',
-  fileApiUrl: 'https://ton-file-service.onrender.com'
+  authApiUrl: 'https://saas-de-gestion-de-projets-type-jira.onrender.com',
+  projectApiUrl: 'https://saas-de-gestion-de-projets-type-jira.onrender.com',
+  fileApiUrl: 'https://saas-de-gestion-de-projets-type-jira.onrender.com'
 };
